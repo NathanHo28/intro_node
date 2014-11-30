2037b25297b1ad946537e5ac1283079e7ca90d1b
NATHAN <nathan.ho28@gmail.com>
2014-11-30 11:51:14 -0500

    added comment for future reference

--- learnyounode/filtered_ls.js
+++ learnyounode/filtered_ls.js
@@ -4,6 +4,7 @@ var dirName = process.argv[2];
 var extension = process.argv[3];
 // for explanation on below: http://www.brent-noorda.com/nombas/us/devspace/manual/c/html/TH_888.htm
 var extFilter = RegExp('\\.' + extension + '$');
+// $ ensures that the extension matches letter sequence only at the end of filename.
 
 fs.readdir(dirName, function (err, list) {
   list.forEach(function (item){
